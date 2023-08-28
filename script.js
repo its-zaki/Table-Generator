68af3cbb7a8b5724f332007d5ccfbeec789ce807
its zaki <zakiyounus789@gmail.com>
2023-08-28 22:40:41 +0500

    Table Generator

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function create_table(){
     let div = document.querySelector("#table");
-    let input = (document.querySelector(".input").value);
+    let input = (document.querySelector("#Input").value);
     div.innerHTML = ''
+    if( Input.value  <= 0 || Input.value == ''   ){
+        
+        div.innerHTML = "Enter Positive Number"
+    }
+    else{
+        for (i=1; i<=10; i++ ){
+           div.innerHTML += `${input} * ${i} = ${input*i} <br/>`
+           
+       }
+     
 
- for (i=1; i<=10; i++ ){
-    div.innerHTML += `${input} * ${i} = ${input*i} <br/>`
-    
-}
+}   
 }
